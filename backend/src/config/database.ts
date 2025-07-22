0a53445c39cac47f204d96c6d7cb5c82ea590756
ejsadiarin <ejs@ejsadiarin.com>
2025-07-23 00:11:24 +0800

    refactor: move database configs to configs

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,8 +6,9 @@ export default new DataSource({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT ?? '3306', 10) || 3306,
   username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  password: process.env.DB_PASSWORD || 'pass',
   database: process.env.DB_DATABASE || 'technest_db',
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
 });
+
